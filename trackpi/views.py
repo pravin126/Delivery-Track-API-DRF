b7208053ea1e21b3498f09d8ff6cbced593b8a57
Pravin Kalokhe <pravinkalokhe5@gmail.com>
2024-06-10 14:51:56 +0530

    Api set

--- trackpi/views.py
+++ trackpi/views.py
@@ -1,6 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from decouple import config
+import requests
+import json
 
+def auth_fedex():
+    try:
+        #Input Data
+        data={
+            'grant_type': 'client_credentials',
+            'client_id':config('FEDEX_API_KEY'),
+            'client_secret':config('FEDEX_SECRET_KEY')
+        }
+        #Heder
+        headers = {
+            'Content-Type': "application/x-www-form-urlencoded"
+            }
+        #make API Call
+        response=response = requests.post(f"{config('FEDEX_BASE_API_URL')}/oauth/token", data=data, headers=headers)
+        return response.json()
+    except Exception as e:
+        print('Error authenticating with FedEx API:',e)
+        raise ValueError('Failed to authenticate with FedEx API')
+    
 class FedexTrackingView(APIView):
     def get(self,req):
-        return Response({"status":"ok"})
+        authRes=auth_fedex()
+        return Response({"auth Res":authRes})
